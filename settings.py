87ef5e3af44cb26629ff7a963bf68aafe1f8608a
jlgray <jgray@.(none)>
2012-04-09 01:33:28 -0400

    Added PROJECT_PATH, settings_env, and a database

--- settings.py
+++ settings.py
@@ -1,6 +1,8 @@
 # Django settings for androidicus project.
 import os
 
+PROJECT_PATH = os.path.dirname(os.path.realpath(__file__))
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -11,14 +13,12 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
+        'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'androidicus',
+        'USER':  'postgres',
+        'PASSWORD': 'secret'
+    },
 }
 
 # Local time zone for this installation. Choices can be found here:
@@ -94,8 +94,16 @@ INSTALLED_APPS = (
     #'django.contrib.staticfiles',
     'home',
     'toys',
+    'wordmorph',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
+
+
+#Overwrite default values with environment specific values
+try:
+    from settings_env import *
+except ImportError:
+    print """\nYour project needs a file named 'settings_env.py'\n Copy 'settings_env.py.sample' and remove the last extension.\n Do not commit your personal 'settings_env.py'\n"""
