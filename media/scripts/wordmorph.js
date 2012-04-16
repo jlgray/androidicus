0cd43f3f38e1eedb7af6cb122f5bf414a72c2b1a
jlgray <jgray@.(none)>
2012-04-16 00:18:56 -0400

    Changed wordmorph over to using templates and forms instead of js

--- media/scripts/wordmorph.js
+++ media/scripts/wordmorph.js
@@ -1,17 +1,10 @@
-function render(html){
-    //console.log(html);
-
+function render(){
     var $wordmorph = $("#wordmorph");
-    $wordmorph.empty();
-
-    $wordmorph.html(html);
-
     var $words = $wordmorph.find(".wordmorph-word");
     var word_height = $words.eq(0).height();
     var timeouts = [];
 
     var dur = 900;
-    var start_time = (new Date()).getTime();
 
     function animate_word($word, i){
         $word.animate({
@@ -21,58 +14,12 @@ function render(html){
                     top: (i+1)*word_height+"px"
                 },dur);
         }});
-//        console.log(i+1, $word, $words.slice(i+1));
     }
 
     $wordmorph.find(".wordmorph-word").each(function(i){
         timeouts[timeouts.length] = setTimeout(animate_word, i*dur, $(this), i);
         i++;
     });
-    /*
-    var $word_base = $("#wordmorph-word");
-    var i, path_len = path.length;
-    var timeouts = [];
-
-    function animate_word($word){
-        $word.animate({opacity: 1.0}, 800);
-    }
-
-    function render_word(this_word, last_word){
-        var $word = $word_base.clone();
-        var pos_diff = diff_pos(this_word, last_word);
-        var word_str;
-
-        if (pos_diff === false)
-            word_str = this_word;
-        else
-            word_str = this_word.slice(0,pos_diff) +
-                "<strong class='changed-letter'>"+this_word[pos_diff]+"</strong>" +
-                this_word.slice(pos_diff+1, this_word.length);
-        $word.html(word_str);
-        $wordmorph.append($word);
-        timeouts[timeouts.length] = setTimeout(animate_word, i*500, $word);
-    }
-
-    render_word(path[0], path[0]);
-
-    for (i=1; i<path_len; i++){
-        render_word(path[i], path[i-1])
-    }
-    */
-}
-
-function get_path(start_word, end_word){
-    $.ajax({
-        type: "GET",
-        url: "/wordmorph/path/",
-        data: {start_word: start_word,
-               end_word: end_word},
-        dataType: "html",
-        success: function(resp){
-            render(resp);
-        }
-    });
-    //return ['art', 'ant', 'and', 'end'];
 }
 
 function diff_pos(word1, word2){
@@ -84,9 +31,5 @@ function diff_pos(word1, word2){
 }
 
 $(document).ready(function(){
-    $("#start-button").click(function(){
-        var start_word = $("#start-word").val();
-        var end_word = $("#end-word").val();
-        get_path(start_word, end_word);
-    });
+    render();
 });
