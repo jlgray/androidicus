58364a4e36e0900042707608f0e7ef1d7f734817
jlgray <jgray@.(none)>
2012-04-26 10:24:53 -0400

    Build graph in database using an undirected adjacency list that treats AB == BA

--- wordmorph/models.py
+++ wordmorph/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
-class WordNode(models.Model):
-    text = models.CharField(max_length=31, db_index=True)
-    neighbors = models.ManyToManyField('self', symmetrical=True)
-    visited = models.BooleanField(default=False)
+class EdgeList(models.Model):
+    word = models.CharField(max_length=32, db_index=True)
+    neighbor =  models.CharField(max_length=32, db_index=True)
+
 
 class BadWord(models.Model):
-    word = models.ForeignKey(WordNode)
+    word = models.CharField(max_length=32, db_index=True)
     
